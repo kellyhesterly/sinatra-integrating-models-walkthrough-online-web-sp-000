6cc9ca53333a9110d464db76bca4d1f949eec95e
Kelly Hesterly <kelly.hesterly@gmail.com>
2021-04-21 17:59:34 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,3 +1,4 @@
+
 require_relative 'config/environment'
 require_relative 'models/text_analyzer.rb'
 
@@ -8,6 +9,7 @@ class App < Sinatra::Base
 
   post '/' do
     @analyzed_text = TextAnalyzer.new(params[:user_text])
+
     erb :results
   end
 end
